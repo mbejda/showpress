fcde75b2d47a544a35352d0d2edf27f346924efa
mbejda <mbejda@live.com>
2013-12-31 01:34:05 -0500

    node

--- app/controllers/pages_controller.js
+++ app/controllers/pages_controller.js
@@ -2,10 +2,30 @@ var locomotive = require('locomotive')
   , Controller = locomotive.Controller;
 
 var PagesController = new Controller();
+var sys = require('sys')
+var exec = require('child_process').exec;
+var child;
 
 PagesController.main = function() {
   this.title = 'Locomotive'
+
+// http://nodejs.org/api.html#_child_processes
+
+
+// executes `pwd`
+child = exec("wp core download", function (error, stdout, stderr) {
+  sys.print('stdout: ' + stdout);
+  sys.print('stderr: ' + stderr);
+  if (error !== null) {
+    console.log('exec error: ' + error);
+  }
+});
+
+
+
+
   this.render();
+
 }
 
 module.exports = PagesController;
