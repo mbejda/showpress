109551f013a623643d5c94382634fa8175d8d798
mbejda <mbejda@live.com>
2013-12-31 20:17:05 -0500

    dd

--- shell.sh
+++ shell.sh
@@ -5,6 +5,6 @@ cd $1
 wp core download
 wp core config --dbname=$1 --dbuser=root
 wp db create 
-wp core install --url='http://162.243.15.132/press/$1' --admin_user='admin' --title='title' --admin_email='mbejda@live.com' --admin_password='password'
+wp core install --url="http://162.243.15.132/press/$1" --admin_user='admin' --title='title' --admin_email='mbejda@live.com' --admin_password='password'
 
 #wp core download
